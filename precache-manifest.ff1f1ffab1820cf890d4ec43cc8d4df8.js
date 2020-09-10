84cd7fcf7f14cfc97a3b68e013eb1fcf045d05aa
Manuel <manuel_8ale@hotmail.com>
2020-09-10 17:36:48 -0400

    Updates

---
+++ precache-manifest.ff1f1ffab1820cf890d4ec43cc8d4df8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17febfac945904831b0e48cd8fd3ea96",
+    "url": "/ecommerce-localdata/index.html"
+  },
+  {
+    "revision": "3de5a21dbc814a7bb7df",
+    "url": "/ecommerce-localdata/static/css/2.547735e1.chunk.css"
+  },
+  {
+    "revision": "27f65807ea3a13461d65",
+    "url": "/ecommerce-localdata/static/css/main.c150de26.chunk.css"
+  },
+  {
+    "revision": "3de5a21dbc814a7bb7df",
+    "url": "/ecommerce-localdata/static/js/2.6d5a74a2.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ecommerce-localdata/static/js/2.6d5a74a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27f65807ea3a13461d65",
+    "url": "/ecommerce-localdata/static/js/main.228306e8.chunk.js"
+  },
+  {
+    "revision": "e886aa1fa8e836f501d4",
+    "url": "/ecommerce-localdata/static/js/runtime-main.f798de9a.js"
+  },
+  {
+    "revision": "d228218ae6d9e97fabed42002912925f",
+    "url": "/ecommerce-localdata/static/media/fruits.d228218a.jpg"
+  },
+  {
+    "revision": "a92ddfbf5f38be58eec7bc2f0b295984",
+    "url": "/ecommerce-localdata/static/media/grains.a92ddfbf.jpg"
+  },
+  {
+    "revision": "94a8069940f536929fcddae66857cdb5",
+    "url": "/ecommerce-localdata/static/media/nuts.94a80699.jpg"
+  },
+  {
+    "revision": "31473a21b05e489d4b8a934ece6e4589",
+    "url": "/ecommerce-localdata/static/media/pseudobanner52.31473a21.jpg"
+  },
+  {
+    "revision": "e53e2b4c2cc077b94a1766f05460eed8",
+    "url": "/ecommerce-localdata/static/media/pseudobanner62.e53e2b4c.jpg"
+  },
+  {
+    "revision": "727bbb832c4f6f8a67ddf64ed42ac639",
+    "url": "/ecommerce-localdata/static/media/pseudobanner7.727bbb83.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ecommerce-localdata/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ecommerce-localdata/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ecommerce-localdata/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ecommerce-localdata/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "262d0e187c2d8c8399a4f44f43e4cea3",
+    "url": "/ecommerce-localdata/static/media/vegetables.262d0e18.jpg"
+  }
+]);
